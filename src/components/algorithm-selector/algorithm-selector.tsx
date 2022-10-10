fca7d7220f3e649d0cc946daec812585d0fc95f8
amir4rab <60965542+amir4rab@users.noreply.github.com>
2022-10-10 14:59:28 +0000

    added AlgorithmSelector

---
+++ src/components/algorithm-selector/algorithm-selector.tsx
@@ -0,0 +1,62 @@
+import SchedulingAlgorithm from '../../types/scheduling-algorithm';
+import classes from './algorithm-selector.module.scss';
+
+interface Algorithm {
+  name: string;
+  id: SchedulingAlgorithm;
+  shortInfo: string;
+}
+
+const algorithms: Algorithm[] = [
+  {
+    id: 'sjf',
+    name: 'Shortest Job First',
+    shortInfo:
+      'Shortest job first (SJF) or shortest process next (SPN), is a scheduling policy that selects for execution the waiting process with the smallest execution time. SJN is a non-preemptive algorithm. Shortest remaining time is a preemptive variant of SJN.',
+  },
+  {
+    id: 'fcfs',
+    name: 'First Come First Served',
+    shortInfo:
+      'First in, first out (FIFO), also known as first come, first served (FCFS), is the simplest scheduling algorithm. FIFO simply queues processes in the order that they arrive in the ready queue. This is commonly used for a task queue, for example as illustrated in this section.',
+  },
+  {
+    id: 'round-robin',
+    name: 'Round Robin',
+    shortInfo:
+      'The scheduler assigns a fixed time unit per process, and cycles through them. If process completes within that time-slice it gets terminated otherwise it is rescheduled after giving a chance to all other processes.',
+  },
+  {
+    id: 'priority',
+    name: 'Priority Scheduling',
+    shortInfo:
+      'Earliest deadline first (EDF) or least time to go is a dynamic scheduling algorithm used in real-time operating systems to place processes in a priority queue. Whenever a scheduling event occurs (a task finishes, new task is released, etc.), the queue will be searched for the process closest to its deadline, which will be the next to be scheduled for execution.',
+  },
+];
+
+interface Props {
+  onSubmit: (v: SchedulingAlgorithm) => void;
+}
+
+const AlgorithmSelector = ({ onSubmit }: Props) => {
+  return (
+    <div>
+      <h3 className={classes.title}>Please select a Scheduling Algorithm</h3>
+      <div>
+        {algorithms.map(({ id, name, shortInfo }) => (
+          <div key={id} className={classes.item}>
+            <h4 className={classes.title}>{name}</h4>
+            <p className={classes.about}>{shortInfo}</p>
+            <div className={classes.submitWrapper}>
+              <button onClick={() => onSubmit(id)} className={classes.submit}>
+                Choose
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default AlgorithmSelector;
