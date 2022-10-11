d2c7190d5e48b10eb8d32b16217688db456410bb
amir4rab <60965542+amir4rab@users.noreply.github.com>
2022-10-11 07:42:37 +0000

    added dialog

---
+++ src/components/dialog/dialog.tsx
@@ -0,0 +1,42 @@
+import type { ComponentChildren } from 'preact';
+import { useRef, useEffect, StateUpdater } from 'preact/hooks';
+import classes from './dialog.module.scss';
+
+interface Props {
+  state: boolean;
+  children: ComponentChildren;
+  title: string;
+  setState: StateUpdater<boolean>;
+}
+
+const Dialog = ({ children, state, title, setState }: Props) => {
+  const elRef = useRef<HTMLDialogElement | null>(null);
+
+  useEffect(() => {
+    let timeOut: undefined | number;
+    console.log(state);
+    if (state) {
+      elRef.current && elRef.current.showModal();
+    } else {
+      timeOut = setTimeout(() => elRef.current && elRef.current.close(), 500);
+    }
+    () => {
+      console.log('Clearing');
+      timeOut && clearTimeout(timeOut);
+    };
+  }, [state]);
+
+  return (
+    <dialog data-displayed={state} className={classes.dialog} ref={elRef}>
+      <div className={classes.header}>
+        <p className={classes.title}>{title}</p>
+        <button className={classes.close} onClick={() => setState(false)}>
+          <img src="/icons/close.svg" />
+        </button>
+      </div>
+      <div className={classes.content}>{children}</div>
+    </dialog>
+  );
+};
+
+export default Dialog;
