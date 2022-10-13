f59bf7ca343062ca183578a4c8abc044b28d5ed3
amir4rab <60965542+amir4rab@users.noreply.github.com>
2022-10-13 07:08:34 +0000

    added gannt chart

---
+++ src/components/result-displayer/gantt-chart/gantt-chart.tsx
@@ -0,0 +1,60 @@
+import Gantt from '../../../types/gannt';
+import classes from './gantt-chart.module.scss';
+
+const ChartItem = ({
+  end,
+  endTime,
+  index,
+  name,
+  start,
+}: {
+  start: number;
+  end: number;
+  name: string;
+  endTime: number;
+  index: number;
+}) => {
+  return (
+    <div
+      className={classes.item}
+      style={`width: ${(endTime / (end - start)) * 100}%`}
+    >
+      <p className={classes.title}>{name}</p>
+      {index === 0 && <p className={classes.leftIndicator}>{start}</p>}
+      {<p className={classes.rightIndicator}>{end}</p>}
+    </div>
+  );
+};
+
+const GanttChart = ({ gantt }: { gantt: Gantt }) => {
+  return (
+    <div>
+      <p>Gantt chart</p>
+      <div className={classes.ganttChart}>
+        {gantt.map(({ endTime, id, processName, startTime }, i) => (
+          <>
+            {i !== 0 && startTime !== gantt[i - 1].endTime && (
+              <ChartItem
+                start={gantt[i - 1].startTime}
+                end={startTime}
+                endTime={gantt[gantt.length - 1].endTime}
+                index={i}
+                name="Free time"
+              />
+            )}
+            <ChartItem
+              end={endTime}
+              start={startTime}
+              name={processName}
+              endTime={gantt[gantt.length - 1].endTime}
+              index={i}
+              key={id}
+            />
+          </>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default GanttChart;
---
+++ src/components/result-displayer/gantt-chart/index.tsx
@@ -0,0 +1,1 @@
+export { default } from './gantt-chart';
