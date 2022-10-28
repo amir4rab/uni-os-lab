04509e1d5879bcf7572ed1b867f312d12c2e1da7
amir arab <60965542+amir4rab@users.noreply.github.com>
2022-10-28 08:16:41 +0000

    initialized pwa setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,40 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     target: 'ES2015',
   },
-  plugins: [preact()],
+  plugins: [
+    preact(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'CPU Scheduler Emulator',
+        short_name: 'CSE',
+        description: 'Simple CPU scheduler emulator',
+        theme_color: '#0ce7d5',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
 });
