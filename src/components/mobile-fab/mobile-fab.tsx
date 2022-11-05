f7bfaaeb0d06261b5aab92ba4088ad30404ad2c1
amir arab <60965542+amir4rab@users.noreply.github.com>
2022-11-05 07:26:48 +0000

    added mobile fab

---
+++ src/components/mobile-fab/mobile-fab.tsx
@@ -0,0 +1,30 @@
+import { ComponentChildren } from 'preact';
+import { useState } from 'preact/hooks';
+import Dialog from '../dialog';
+import classes from './mobile-fab.module.scss';
+
+interface Props {
+  title: string;
+  icon: string;
+  children: ComponentChildren;
+}
+
+const MobileFab = ({ icon, title, children }: Props) => {
+  const [displayed, setDisplayed] = useState(false);
+
+  return (
+    <>
+      <button
+        onClick={() => setDisplayed((curr) => !curr)}
+        className={[classes.fabButton, 'primary'].join(' ')}
+      >
+        {icon}
+      </button>
+      <Dialog title={title} state={displayed} setState={setDisplayed}>
+        {children}
+      </Dialog>
+    </>
+  );
+};
+
+export default MobileFab;
