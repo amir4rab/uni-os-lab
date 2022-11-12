e8a790e98cac1846c46b2f0a3e3691b24947868c
amir arab <60965542+amir4rab@users.noreply.github.com>
2022-11-12 06:53:51 +0000

    added super lazy loading to iframe

--- src/components/learn-more/learn-more.tsx
+++ src/components/learn-more/learn-more.tsx
@@ -1,6 +1,9 @@
+import { useState } from 'preact/hooks';
 import classes from './learn-more.module.scss';
 
 const learnMore = () => {
+  const [loadIframe, setLoadIframe] = useState(false);
+
   return (
     <div className={classes.learnMore}>
       <h3>Learn more</h3>
@@ -20,16 +23,26 @@ const learnMore = () => {
         <span>{'. Or you could watch the following video on youtube.'}</span>
       </p>
       <div className={classes.iframeWrapper}>
-        <iframe
-          loading="lazy"
-          width="560"
-          height="315"
-          src="https://www.youtube-nocookie.com/embed/Jkmy2YLUbUY"
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
+        {!loadIframe ? (
+          <div
+            className={classes.iframeLoader}
+            style="max-height: 315px; max-width: 560px; aspect-ratio: 560 / 315;"
+          >
+            <p>keep in mind loading youtube will lead to data usage.</p>
+            <button onClick={() => setLoadIframe(true)}>Load</button>
+          </div>
+        ) : (
+          <iframe
+            loading="lazy"
+            width="560"
+            height="315"
+            src="https://www.youtube-nocookie.com/embed/Jkmy2YLUbUY"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        )}
       </div>
     </div>
   );
