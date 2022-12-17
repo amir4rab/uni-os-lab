ae73aeb66219fe3c10a6e05a5c6f9253602a0fa4
amir4rab <60965542+amir4rab@users.noreply.github.com>
2022-12-17 06:16:27 +0100

    updates to select component

--- src/components/select/select.tsx
+++ src/components/select/select.tsx
@@ -2,22 +2,31 @@ import { MutableRef } from "preact/hooks";
 import classes from "./select.module.scss";
 
 interface Props {
-  name: string;
+  name?: string;
   type?: string;
   id?: string;
+  marginLess?: boolean;
   defaultValue?: string;
   passedRef?: MutableRef<null | HTMLSelectElement>;
   required?: boolean;
+  onUpdate?: (v: string) => void
   options: {
     value: string;
     name: string
   }[]
 };
 
-const Select = ({ options, name , passedRef, ...props }: Props) => (
-  <div className={classes.selectWrapper}>
-    <label htmlFor={name}>{ name }</label>
-    <select ref={passedRef} {...props}>
+const Select = ({ options, name , passedRef, marginLess, onUpdate, ...props }: Props) => (
+  <div className={classes.selectWrapper} data-margin-less={marginLess}>
+    { name && <label htmlFor={name}>{ name }</label> }
+    <select
+      onChange={(e) => {
+        const v = (e.target as HTMLSelectElement).value;
+        onUpdate && onUpdate(v);
+      }}
+      ref={passedRef} 
+      {...props}
+    >
       {
         options.map(({ name, value }) => (
           <option value={ value } key={ value }>
